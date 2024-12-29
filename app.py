22b774b5bbe9a323ce9173e38e21fb7948590470
agastyawe45 <agastya.reddy@we45.com>
2024-12-29 16:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,10 @@ def register_page():
 def home_page():
     return render_template("home.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 # Serve Static Files
 @app.route("/static/<path:filename>")
 def static_files(filename):
